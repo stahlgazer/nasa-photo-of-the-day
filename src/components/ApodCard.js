63a685a58fa6ecc145c52255b0f99102870cf8fc
gavin stahl <stahlgavin@gmail.com>
2019-12-12 14:23:30 -0800

    added some reactstrap stylings

--- src/components/ApodCard.js
+++ src/components/ApodCard.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Badge, Alert, Spinner, Collapse } from 'reactstrap'
 
 const ApodCard = props => {
     // console.log(props)
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <div className="apod-card">
-            <h1>Astronomy Picture of the Day</h1>
-            <p><a href="https://apod.nasa.gov/apod/archivepix.html">Discover the cosmos!</a> Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</p>
-            <p>{props.date}</p>
-            <img src={props.url} alt="daily astronomy pic"></img>
-            <p>{props.title}</p>
-            <p>Explanation: {props.explanation}</p>
+            <h1><Badge color="primary">New</Badge> Astronomy Picture of the Day</h1>
+
+            <a href="https://apod.nasa.gov/apod/archivepix.html"><Button color="info">Discover the cosmos!</Button></a>
+            
+            <Alert color="info">Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.</Alert>
+
+            <p><Spinner color="primary" /> {props.date} <Spinner color="primary" /></p>
+
+            <img src={props.url} alt="daily astronomy pic" width="60%"></img>
+
+            <Alert>{props.title}</Alert>
+
+            <Button color="info" onClick={toggle} style={{ marginBottom: '1rem' }}>Explanation</Button>
+            <Collapse isOpen={isOpen}>{props.explanation} </Collapse>
         </div>
     );
 };
